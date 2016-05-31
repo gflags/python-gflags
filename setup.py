d237e798e8624fe5a5e01329506c5704c4f4dce2
Josh Williams <jwilliams@endpoint.com>
2016-05-30 22:50:55 -0400

    Bump version for Python 3 support

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@
 from setuptools import setup
 
 setup(name='python-gflags',
-      version='2.0',
+      version='3.0',
       description='Google Commandline Flags Module',
       license='BSD',
       author='Google Inc. and others',
